0e17aaa033d034169cfe4c95b3d50ca9df0bb2aa
makarios fared <makarios.fared@gmail.com>
2026-03-04 17:37:08 +0200

    Implement HomePage with tap-to-change background color

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_color_changer/core/color_generation.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final ColorGeneration _colorGeneration = ColorGeneration();
+  Color _backgroundColor = Colors.white;
+
+  void _changeColor() {
+    setState(() {
+      _backgroundColor = _colorGeneration.generateRandomColor();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          "Hello there",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    return GestureDetector(
+      onTap: _changeColor,
+      child: Scaffold(
+        backgroundColor: _backgroundColor,
+        body: GestureDetector(
+          onTap: _changeColor,
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 500),
+            child: Center(
+              child: Text(
+                "Hello there",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
         ),
       ),
     );
