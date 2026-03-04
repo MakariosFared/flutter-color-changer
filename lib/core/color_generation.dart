f5c820eeb955bda2e637e22fde4d8062724c6a82
makarios fared <makarios.fared@gmail.com>
2026-03-04 19:10:09 +0200

    refactor: improve ColorGeneration to comply with solid_lints

--- lib/core/color_generation.dart
+++ lib/core/color_generation.dart
@@ -1,13 +1,17 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 
+/// A class responsible for generating random RGB colors.
 class ColorGeneration {
   final Random _random = Random();
 
+  /// Generates a random color using RGB values (0–255).
   Color generateRandomColor() {
-    int r = _random.nextInt(256);
-    int g = _random.nextInt(256);
-    int b = _random.nextInt(256);
-    return Color.fromARGB(255, r, g, b);
+    const int a = 255;
+    final int r = _random.nextInt(256);
+    final int g = _random.nextInt(256);
+    final int b = _random.nextInt(256);
+
+    return Color.fromARGB(a, r, g, b);
   }
 }
